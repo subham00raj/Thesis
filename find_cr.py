1bf408061fbbc1186785418b95b2d95a4849a32a
subham00raj <subham00raj@gmail.com>
2024-08-20 15:34:50 +0530

    Updated

--- find_cr.py
+++ find_cr.py
@@ -12,7 +12,10 @@ Reference : https://uavsar.jpl.nasa.gov/cgi-bin/calibration.pl
 
 '''
 import os
+import read_image
 import wget
+import cv2
+import math
 from datetime import datetime
 import requests
 import struct
@@ -151,23 +154,6 @@ def create_inc_array_flat(min_look_angle = 21.32159622, max_look_angle = 66.1712
     stacked_inc_array = np.tile(array, (row_1x1, 1))
     return np.fliplr(stacked_inc_array)
 
-
-def read_slc(file_path, rows = 61349, cols = 9874, gpu = False):
-    image = np.memmap(file_path, shape = (rows, cols) ,dtype = np.complex64)
-    
-    if gpu:
-        import torch
-        device = torch.device('cuda')
-        array = torch.from_numpy(image).to(device)
-        result = array.cpu()
-        del array
-        torch.cuda.empty_cache()
-        return result
-
-    else:
-        return np.array(image)
-    
-
 def get_cr_location(llh_file_path):
     now = datetime.now()
     csv_path = f'{now.strftime("%Y")}-{now.strftime("%m")}-{now.strftime("%d")}_0000_Rosamond-corner-reflectors_with_plate_motion.csv'
@@ -181,6 +167,60 @@ def get_cr_location(llh_file_path):
 
         pixel_loc.append(llh_df[(llh_df['CR_Latitude'] < 0.00001) & (llh_df['CR_Longitude'] < 0.0001)].index.tolist())
 
-
     return pixel_loc
+
+def get_dataframe(image, llh_file_path):
+    now = datetime.now()
+    csv_path = f'{now.strftime("%Y")}-{now.strftime("%m")}-{now.strftime("%d")}_0000_Rosamond-corner-reflectors_with_plate_motion.csv'
+    cr_df = pd.read_csv(csv_path)
+    df = cr_df.copy()
+        
+    def calculate_indices(total_cols, count):
+        row_index = (count - 1) // total_cols
+        col_index = (count - 1) % total_cols
+        return (row_index, col_index)
+
+    def calculate_location_in_slc(loc):
+        ml_row, ml_col = loc[0],loc[1]
+        ml_factor_row, ml_factor_col = 8,2
+        slc_row = ml_row * ml_factor_row
+        slc_col = ml_col * ml_factor_col
+        return slc_row, slc_col
+
+    def exact_location(X, Y, window_size, image):
+        top_left_x = X - window_size // 2
+        top_left_y = Y - window_size // 2
+        window = image[top_left_y:top_left_y + window_size, top_left_x:top_left_x + window_size]
+        max_pixel_coordinates = np.unravel_index(np.argmax(window), window.shape)
+        return top_left_y + max_pixel_coordinates[0], top_left_x + max_pixel_coordinates[1]
+
+    total_cols = 4937
+    df['Pixel Number'] = get_cr_location(llh_file_path)
+    df['Pixel Location in 2x8 SLC'] = df['Pixel Number'].apply(lambda x: [calculate_indices(total_cols, i) for i in x])
+    df['Pixel Location in 1x1 SLC'] = df['Pixel Location in 2x8 SLC'].apply(lambda x: [calculate_location_in_slc(i) for i in x])
+    df['Pixel Location in Local SLC'] = df['Pixel Location in 1x1 SLC'].apply(lambda x: [(i[0]-41000, i[1]-2000) for i in x])
+
+    df['Tentative Location'] = df['Pixel Location in Local SLC'].apply(lambda x: x[x.index(max(x))])
+    df['Tentative Y'] = df['Tentative Location'].apply(lambda x: x[0])
+    df['Tentative X'] = df['Tentative Location'].apply(lambda x: x[1])
+
+    df['Y'] = [exact_location(df['Tentative X'][i],df['Tentative Y'][i], window_size=16, image = image)[0] for i in range(len(df))]
+    df['X'] = [exact_location(df['Tentative X'][i],df['Tentative Y'][i], window_size=16, image = image)[1] for i in range(len(df))]
+
+    return df[['Corner ID', 'Latitude', 'Longitude', 'Height', 'Azimuth', 'Elevation Angle', 'Side Length', 'Y', 'X']]
+
+
+if __name__ == '__main__':
+    image_path = r'C:\Users\Vision IAS\Desktop\work\HH.tiff'
+    llh_file_path = r'C:\Users\Vision IAS\Desktop\work\Thesis\Rosamd_35012_04_BC_s1_2x8.llh'
+    image = read_image.image(image_path, start_coordinate = [4500, 17500], image_size = [3500, 3500], return_array=True)
+    df = get_dataframe(image, llh_file_path)
+
+    for i in range(len(df)):
+        cv2.circle(image, (df['X'][i],df['Y'][i]), 50, 1, 1)
+        
+    plt.imshow(image, cmap='gray',vmin=1e-5,vmax=0.2)
+    plt.show()
+
+
 
--- read_image.py
+++ read_image.py
@@ -34,17 +34,13 @@ def create_image(slc, type=None):
         return type_dict[type](slc)
     raise ValueError(f"Invalid datatype specified: {type}. Choose from: 'amplitude', 'imaginary', 'magnitude', 'intensity', 'power'.")
 
-def image(image_path, start_coordinate, image_size, return_array=False, min_threshold=None, max_threshold=None):
+def image(image_path, start_coordinate, image_size):
     dataset = gdal.Open(image_path)
     band = dataset.GetRasterBand(1)
     image_subset = band.ReadAsArray(start_coordinate[0], start_coordinate[1], image_size[0], image_size[1])
+    return image_subset
 
-    if return_array:
-        return image_subset
-    plt.imshow(image_subset, cmap='gray', vmin=min_threshold, vmax=max_threshold)
-    plt.show()
-
-def multilooked(image, range_pixel=1.6, azimuth_pixel=0.6, return_array=False):
+def multilooked(image, range_pixel=1.6, azimuth_pixel=0.6):
     factor = (int(range_pixel * azimuth_pixel * 100) // math.gcd(int(range_pixel * 10), int(azimuth_pixel * 10))) / 10
     range_looks = int(np.ceil(factor / range_pixel))
     azimuth_looks = int(np.ceil(factor / azimuth_pixel))
@@ -60,13 +56,22 @@ def multilooked(image, range_pixel=1.6, azimuth_pixel=0.6, return_array=False):
                 multilook_image[i, j] = np.mean(image[start_row:end_row, start_col:end_col])
                 progress_bar.update(1)
 
-    if return_array:
-        return multilook_image
-    plt.imshow(multilook_image, cmap='gray', vmin=0.001, vmax=0.12)
-    plt.show()
+    return multilook_image
+
 
 if __name__ == '__main__':
-    coord = [4500, 17500]  # Starting coordinate
-    size = [3500, 3500]    # Image size
-    img = image('HH.tiff', start_coordinate=coord, image_size=size, return_array=True)
-    multilooked(image=img, range_pixel=1.6, azimuth_pixel=0.6)
+    coord = [4500, 17500]  # Starting coordinate [x,y]
+    size = [3500, 3500]    # Image size [x,y]
+    image_path = r'C:\Users\Vision IAS\Desktop\work\HH.tiff'
+    img1 = image(image_path, start_coordinate=coord, image_size=size)
+    img2 = multilooked(image=img1, range_pixel=1.6, azimuth_pixel=0.6)
+    
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    axs[0].imshow(img1, cmap='gray', vmin=0.001, vmax=0.13)
+    axs[0].set_title('Raw Distorted Image - Rectangular Pixels')
+    
+    axs[1].imshow(img2, cmap='gray', vmin=0.001, vmax=0.13)
+    axs[1].set_title('Geometrically Corrected Image - Square Pixels')
+
+    plt.show()
+
