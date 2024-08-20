1a2cd1ea614fd9d9dbdf4dd01c5df54fe380271b
subham00raj <subham00raj@gmail.com>
2024-08-20 13:23:39 +0530

    Added Multilooked Function

--- read_image.py
+++ read_image.py
@@ -1,13 +1,14 @@
-from osgeo import gdal
 import tifffile
+import math
+from tqdm import tqdm
 import numpy as np
-gdal.UseExceptions()
+from osgeo import gdal
 import matplotlib.pyplot as plt
 
+gdal.UseExceptions()
 
-def read_slc(file_path, rows = 61349, cols = 9874, gpu = False):
-    image = np.memmap(file_path, shape = (rows, cols) ,dtype = np.complex64)
-    
+def read_slc(file_path, rows=61349, cols=9874, gpu=False):
+    image = np.memmap(file_path, shape=(rows, cols), dtype=np.complex64)
     if gpu:
         import torch
         device = torch.device('cuda')
@@ -16,43 +17,56 @@ def read_slc(file_path, rows = 61349, cols = 9874, gpu = False):
         del array
         torch.cuda.empty_cache()
         return result
-
-    else:
-        return np.array(image)
-    
+    return np.array(image)
 
 def array_to_tiff(array, file_name):
     tifffile.imwrite(file_name, array)
 
-def create_image(slc, type = None):
-    if type == 'amplitude':
-        return np.real(slc)
-    elif type == 'imaginary':
-        return np.imag(slc)
-    elif type == 'magnitude':
-        return np.abs(slc)
-    elif type == 'intensity':
-        return np.abs(slc)**2
-    elif type == 'power':
-        return 20*np.log10(np.abs(slc))
-    else:
-        raise ValueError(f"Invalid datatype specified: {type}. Choose among : 'amplitude', 'imaginary', 'magnitude', 'intensity' or 'power'.")
+def create_image(slc, type=None):
+    type_dict = {
+        'amplitude': np.real,
+        'imaginary': np.imag,
+        'magnitude': np.abs,
+        'intensity': lambda x: np.abs(x)**2,
+        'power': lambda x: 20 * np.log10(np.abs(x))
+    }
+    if type in type_dict:
+        return type_dict[type](slc)
+    raise ValueError(f"Invalid datatype specified: {type}. Choose from: 'amplitude', 'imaginary', 'magnitude', 'intensity', 'power'.")
 
-def plot_image(image_path, start_coordinate, image_size, min_threshold = 0.001, max_threshold = 0.13, return_array = False):
+def image(image_path, start_coordinate, image_size, return_array=False, min_threshold=None, max_threshold=None):
     dataset = gdal.Open(image_path)
-    band = dataset.GetRasterBand(1)  
+    band = dataset.GetRasterBand(1)
     image_subset = band.ReadAsArray(start_coordinate[0], start_coordinate[1], image_size[0], image_size[1])
+
     if return_array:
         return image_subset
-    else:
-        plt.imshow(image_subset, cmap='gray', vmin = min_threshold, vmax = max_threshold)
-        plt.show()
+    plt.imshow(image_subset, cmap='gray', vmin=min_threshold, vmax=max_threshold)
+    plt.show()
 
+def multilooked(image, range_pixel=1.6, azimuth_pixel=0.6, return_array=False):
+    factor = (int(range_pixel * azimuth_pixel * 100) // math.gcd(int(range_pixel * 10), int(azimuth_pixel * 10))) / 10
+    range_looks = int(np.ceil(factor / range_pixel))
+    azimuth_looks = int(np.ceil(factor / azimuth_pixel))
+
+    rows, cols = image.shape[0] // azimuth_looks, image.shape[1] // range_looks
+    multilook_image = np.zeros((rows, cols), dtype=image.dtype)
+
+    with tqdm(total=rows * cols, desc="Progress", unit=" pixels") as progress_bar:
+        for i in range(rows):
+            for j in range(cols):
+                start_row, end_row = i * azimuth_looks, (i + 1) * azimuth_looks
+                start_col, end_col = j * range_looks, (j + 1) * range_looks
+                multilook_image[i, j] = np.mean(image[start_row:end_row, start_col:end_col])
+                progress_bar.update(1)
+
+    if return_array:
+        return multilook_image
+    plt.imshow(multilook_image, cmap='gray', vmin=0.001, vmax=0.12)
+    plt.show()
 
 if __name__ == '__main__':
-    coord = [4500,17500]  # Starting coordinate
-    size = [3500,3500]    # Image size
-    plot_image('HH.tiff', start_coordinate = coord, image_size = size, min_threshold = 0.1, max_threshold = 0.13)
-
-
-
+    coord = [4500, 17500]  # Starting coordinate
+    size = [3500, 3500]    # Image size
+    img = image('HH.tiff', start_coordinate=coord, image_size=size, return_array=True)
+    multilooked(image=img, range_pixel=1.6, azimuth_pixel=0.6)
