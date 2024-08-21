69d4f8ad7e9e7dd9e80e23b3f444445a5ba3e8b2
subham00raj <subham00raj@gmail.com>
2024-08-21 13:09:55 +0530

    added radio_cal

--- radiometric_calibration.py
+++ radiometric_calibration.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def radar_cross_section(length, wavelength, theta, phi):
-  theta = np.deg2rad(theta)
-  phi = np.deg2rad(phi)
+def radar_cross_section(length, wavelength, elevation_angle, theta, azimuth):
+  theta_cr = np.deg2rad(theta + elevation_angle)
+  phi = np.deg2rad(azimuth)
   factor = (4 * np.pi * (length**4)) / wavelength**2
-  a = np.cos(theta) + np.sin(theta) * (np.cos(phi) + np.sin(phi))
+  a = np.cos(theta_cr) + np.sin(theta_cr) * (np.cos(phi) + np.sin(phi))
   b = 2/a
   rcs = factor * ((a - b)**2)
   return rcs**0.5
+
+def absolute_calibration_constant(measured_rcs, theoritical_rcs):
+  return measured_rcs / theoritical_rcs
