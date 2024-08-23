45a4622ba527af312936d8b87dffe3a4e923f7c6
subham00raj <subham00raj@gmail.com>
2024-08-23 22:33:29 +0530

    update

--- radiometric_calibration.py
+++ radiometric_calibration.py
@@ -10,17 +10,38 @@ def radar_cross_section(length, wavelength, elevation_angle, theta, azimuth):
   return rcs
 
 
-def get_point_target_analysis_parameters(length, wavelength, elevation_angle, theta, azimuth, HH, HV, VH, VV):
+def point_target_analysis(length, wavelength, elevation_angle, theta, azimuth, HH, VV):
 
   # correlator gain
   measured_rcs = np.abs(HH)
   theoritical_rcs = radar_cross_section(length, wavelength, elevation_angle, theta, azimuth)
   a = (theoritical_rcs / measured_rcs) ** 0.5
 
-  # co-pol channel imbalance amplitude
+  # co channel imbalance amplitude
   f = ((np.abs(VV)**2) / (np.abs(HH))) ** 0.25
 
-  # phase calibration
-  phase_sum = None
+  # co channel imbalance phase
+  phase_sum = np.angle(np.array(VV) * np.conj(HH))
 
   return a, f, phase_sum
+
+
+def distributed_target_analysis(HV, VH, gpu=False):
+    # Cross-channel imbalance amplitude
+    g = np.mean((np.abs(HV) ** 2) / (np.abs(VH))) ** 0.25
+
+    # Cross-channel imbalance phase
+    phase_diff = np.angle(np.mean(HV * np.conj(VH)))
+
+    if gpu:
+        import torch
+
+        HV_tensor = torch.tensor(HV, device='cuda')
+        VH_tensor = torch.tensor(VH, device='cuda')
+
+        g_gpu = torch.mean((torch.abs(HV_tensor) ** 2) / torch.abs(VH_tensor)) ** 0.25
+        phase_diff_gpu = torch.angle(torch.mean(HV_tensor * torch.conj(VH_tensor)))
+
+        return g_gpu.cpu().numpy(), phase_diff_gpu.cpu().numpy()
+
+    return g, phase_diff
