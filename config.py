da66b79fbe7f369e62c9359c9e4c125e34883d3e
subham00raj <subham00raj@gmail.com>
2024-08-26 10:27:36 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+row_1x1 = 61349
+col_1x1 = 9874
+row_2x8 = 7669
+col_2x8 = 4937
+mininum_look_angle = 21.32159622
+maximum_look_angle = 66.17122143
+subset_start_coordinate = [2000,41000]
+subset_size = [3000,3000]
